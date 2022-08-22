10c63e9abd5705596a1e2f1fa593eb51d52277aa
Melih Özgürün <melihozgurun@gmail.com>
2022-08-22 15:14:45 +0300

    Create tr-TR.js
    Turkish language file

---
+++ packages/src/locale/lang/tr-TR.js
@@ -0,0 +1,20 @@
+export default {
+    pagination: {
+        goto: "Git",
+        page: "",
+        itemsPerPage: " / sayfa",
+        total: (total) => `Toplam ${total}`,
+        prev5: "Önceki 5 sayfa",
+        next5: "Sonraki 5 sayfa",
+    },
+    table: {
+        // filter
+        confirmFilter: "Onay",
+        resetFilter: "Sıfırla",
+        // contextmenu event
+        insertRowAbove: "üstte satır ekle",
+        insertRowBelow: "alta satır ekle",
+        removeRow: "satırı sil",
+        hideColumn: "sütünu gizle",
+    },
+};
